95e9d901aed93582ba464e22d4f05e9a8e802b84
Nam Phùng <nam@NAMs-MacBook-Pro.local>
2025-02-22 11:20:04 +0700

    fix code

--- db.py
+++ db.py
@@ -17,8 +17,8 @@ class EasyMongo:
         Initialize the MongoDB client.
         '''
         self.URI = os.getenv("MONGO_URI")
-        self.DB = os.getenv["GEMINI_DB"]
-        self.COLLECTION = os.getenv["GEMINI_COLLECTION"]
+        self.DB = os.getenv("MONGO_DB")
+        self.COLLECTION = os.getenv("MONGO_COLLECTION")
         
     def get_database(self):
         '''
